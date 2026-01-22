40db90e462bcf5a2d6d647859a192ec031c551d0
Ren Fukuoka <lc25-0318@lclark.edu>
2026-01-21 22:52:33 -0800

    done

--- Lab 1/copy.c
+++ Lab 1/copy.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 
+static int a[2048][2048];
+static int b[2048][2048];
+static int c[2048][2048];
+static int d[2048][2048];
+
 void copy(int src[2048][2048], int dst[2048][2048])
 {
     int i, j;
@@ -41,6 +46,15 @@ int main()
     unsigned long before, delta;
 
     before = time_get();
-    copy();
+    copy(a, b);
     delta = time_get() - before;
+
+    unsigned long before2, delta2;
+
+    before2 = time_get();
+    copy(c, d);
+    delta2 = time_get() - before2;
+
+    printf("copy time: %lu ns\ncopy2 time: %lu ns", delta, delta2);
+    return 0;
 }
