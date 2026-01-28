d7edf397c7738f6957da42254a26b71a930c3ef4
Ren Fukuoka <lc25-0318@lclark.edu>
2026-01-28 14:51:31 -0800

    uhm

--- 1:28:2026/linked_node.c
+++ 1:28:2026/linked_node.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -8,5 +9,26 @@ struct node
 
 int main()
 {
-    struct node *p = (struct node *)malloc(sizeof(struct node));
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    printf("%p\n", ptr);
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "Out of memory! \n");
+        exit(1);
+    }
+    struct node *ptr2 = (struct node *)malloc(sizeof(struct node));
+    printf("%p\n", ptr2);
+    if (ptr2 == NULL)
+    {
+        fprintf(stderr, "Out of memory! \n");
+        exit(2);
+    }
+    struct node *ptr3 = (struct node *)malloc(sizeof(struct node));
+    printf("%p\n", ptr3);
+    if (ptr3 == NULL)
+    {
+        fprintf(stderr, "Out of memory! \n");
+        exit(3);
+    }
+    exit(0);
 }
